54b7e5b149a6b89b0f0f8e13335acab7bd1e07f5
neumann-mlucas <neumann.m.lucas@gmail.com>
2024-12-03 09:42:53 -0300

    formatting

--- src/day03.jl
+++ src/day03.jl
@@ -3,7 +3,7 @@ TEST_INP_P2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?m
 TEST_OUT_P1 = 161
 TEST_OUT_P2 = 48
 
-function run_multiplication(inp:: String)::Int
+function run_multiplication(inp::String)::Int
     total = 0
     for m in eachmatch(r"mul\((\d{1,3}),(\d{1,3})\)", inp)
         total += parse(Int, m[1]) * parse(Int, m[2])
@@ -11,7 +11,7 @@ function run_multiplication(inp:: String)::Int
     total
 end
 
-function run_do_multiplication(inp:: String)::Int
+function run_do_multiplication(inp::String)::Int
     pattern = r"do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)"
 
     total, active = 0, true
@@ -33,7 +33,7 @@ day03p2 = run_do_multiplication
 function main()
     @assert day03p1(TEST_INP_P1) == TEST_OUT_P1
     @assert day03p2(TEST_INP_P2) == TEST_OUT_P2
-    
+
     input = read(open("dat/day03.txt", "r"), String)
     day03p1(input) |> println
     day03p2(input) |> println
