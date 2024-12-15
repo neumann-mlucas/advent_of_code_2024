6b5beae14505a0fdf6d13ab943aeb117ede5ee9b
neumann-mlucas <neumann.m.lucas@gmail.com>
2024-12-15 14:39:34 -0300

    add day 09 p1

--- src/day09.jl
+++ src/day09.jl
@@ -8,7 +8,7 @@ function clean_input(inp::String)::Vector{Int}
     parse.(Int, collect(strip(inp)))
 end
 
-function toblocks(diskmap::Vector{Int}, id::Int = 0, phase::Bool = true)::Vector{Int}
+function toblocks(diskmap::Vector{Int}, id::Int=0, phase::Bool=true)::Vector{Int}
     if length(diskmap) == 0
         return []
     end
@@ -33,19 +33,62 @@ function reorder_blocks(blocks::Vector{Int})::Vector{Int}
     return reorder_blocks(blocks)
 end
 
-function reorder_files(blocks::Vector{Int})::Vector{Int}
-    [1, 2, 3]
-end
-
 function checksum(blocks::Vector{Int})::Int
     total = 0
     for (index, value) in enumerate(blocks)
         if value == -1
-            break
+            continue
         end
         total += (index - 1) * value
     end
     total
+end
+
+function findlastfile(vec::Vector{Int}, start::Int)::Vector{Int}
+    start = findprev(x -> x != -1, vec, start)
+
+    file, idx = [], start
+    while vec[idx] == vec[start]
+        push!(file, idx)
+        idx -= 1
+    end
+    return file
+end
+
+function findspace(vec::Vector{Int}, size::Int)::Vector{Int}
+    space = []
+    for idx in eachindex(vec)
+        if length(space) == size
+            return space
+        end
+
+        if vec[idx] == -1
+            push!(space, idx)
+        else
+            space = []
+        end
+    end
+    return []
+end
+
+function reorder_file(blocks::Vector{Int}, start::Int)::Int
+    file = findlastfile(blocks, start)
+    space = findspace(blocks, length(file))
+
+    if length(space) == length(file) && space[end] < file[1]
+        blocks[space] = blocks[file]
+        blocks[file] .= -1
+    end
+
+    return minimum(file) - 1
+end
+
+function reorder_files(blocks::Vector{Int})::Vector{Int}
+    idx = length(blocks)
+    while idx > 2
+        idx = reorder_file(blocks, idx)
+    end
+    return blocks
 end
 
 day09p1 = checksum ∘ reorder_blocks ∘ toblocks ∘ clean_input
@@ -53,11 +96,11 @@ day09p2 = checksum ∘ reorder_files ∘ toblocks ∘ clean_input
 
 function main()
     @assert day09p1(TEST_INP) == TEST_OUT_P1
-    # @assert day09p2(TEST_INP) == TEST_OUT_P2
+    @assert day09p2(TEST_INP) == TEST_OUT_P2
 
     input = read(open("dat/day09.txt", "r"), String)
     day09p1(input) |> println
-    # day09p2(input) |> println
+    day09p2(input) |> println
 end
 
 main()
