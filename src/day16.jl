3cfb32138081de4ab8d2c057ab3da88b0627cc8d
neumann-mlucas <neumann.m.lucas@gmail.com>
2024-12-20 17:59:42 -0300

    use priority queue

--- src/day16.jl
+++ src/day16.jl
@@ -1,5 +1,4 @@
-using Images
-import Base.Iterators
+using DataStructures
 
 TEST_INP = """
 #################
@@ -31,6 +30,10 @@ const MOVES = [
 ]
 U, D, L, R = MOVES
 
+struct Step
+    loc::CartesianIndex
+    dir::CartesianIndex
+end
 
 function clean_input(inp::String)::Matrix{Char}
     hcat(collect.(split(strip(inp)))...) |> permutedims
@@ -40,25 +43,17 @@ end
     0 < p.I[1] <= size(m)[1] && 0 < p.I[2] <= size(m)[2]
 end
 
-# function print_grid(m::Matrix{Char})
-#     mapslices(join, m, dims=2) .|> println
-#     println()
-# end
-#
-# function print_grid(m::Matrix{Bool})
-#     println("DIST = ", sum(m))
-#     fn(x) = x ? 'X' : '.'
-#     mapslices(join, fn.(m), dims=2) .|> println
-#     println()
-# end
-#
-# function print_grid(m::Matrix{Bool}, p::Int)
-#     println("COST = ", p)
-#     println("DIST = ", sum(m))
-#     fn(x) = x ? 'X' : '.'
-#     mapslices(join, fn.(m), dims=2) .|> println
-#     println()
-# end
+function print_grid(m::Matrix{Char})
+    mapslices(join, m, dims=2) .|> println
+    println()
+end
+
+function print_grid(m::Matrix{Bool})
+    println("DIST = ", sum(m))
+    fn(x) = x ? 'X' : '.'
+    mapslices(join, fn.(m), dims=2) .|> println
+    println()
+end
 
 function get_dist(p::CartesianIndex, q::CartesianIndex)::Int
     if p in (U, D) && q in (L, R)
@@ -74,9 +69,12 @@ function walk_dijkstra(s::CartesianIndex, M::Matrix{Char})
     dist = fill(Inf, size(M)...)
     dist[s] = 0
 
-    Q = [(s, CartesianIndex(0, 0))]
+    Q = PriorityQueue{Step,Int}()
+    Q[Step(s, CartesianIndex(0, 0))] = 0
+
     while !isempty(Q)
-        p, dir = popfirst!(Q)
+        step = dequeue!(Q)
+        p, dir = step.loc, step.dir
 
         if M[p] == 'E'
             return dist[p]
@@ -84,19 +82,16 @@ function walk_dijkstra(s::CartesianIndex, M::Matrix{Char})
 
         for move in MOVES
             q = p + move
-
             if !inbounds(q, M) || M[q] == '#'
                 continue
             end
 
             cost = dist[p] + get_dist(move, dir)
-
             if cost <= dist[q]
                 dist[q] = cost
-                push!(Q, (q, move))
+                Q[Step(q, move)] = cost
             end
         end
-        sort!(Q, by=i -> dist[i[1]])
     end
     return -1
 end
@@ -112,7 +107,7 @@ day16p1(inp) = clean_input(inp) |> get_paths |> Int
 function main()
     @assert day16p1(TEST_INP) == TEST_OUT_P1
     # @assert day16p2(TEST_INP) == TEST_OUT_P2
-    #
+
     input = read(open("dat/day16.txt", "r"), String)
     day16p1(input) |> println
     # day16p2(input) |> println
